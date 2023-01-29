ce271a8e740bc2b99135ccf34ef963a8ffb95906
Saman Sayad <samansayad93@gmail.com>
2023-01-29 11:42:13 +0330

    cards-custom type & receiver func

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Five of Diamonds", newcard()}
+	cards := deck{"Five of Diamonds", newcard()}
 	cards = append(cards, "Ace of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newcard() string {
