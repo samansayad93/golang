ab216a184284475441875d9176a3fd66d3c1af09
Saman Sayad <samansayad93@gmail.com>
2023-01-30 16:15:05 +0330

    cards-shuffle fix

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of deck
@@ -58,8 +59,11 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newposition := rand.Intn(len(d) - 1)
+		newposition := r.Intn(len(d) - 1)
 
 		d[i], d[newposition] = d[newposition], d[i]
 	}
