5add070e6af2c26f009e65907675ae59110c4b9e
Saman Sayad <samansayad93@gmail.com>
2023-02-08 17:27:13 +0330

    struct-nested structs

--- structs/main.go
+++ structs/main.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
-	alex.firstName = "John"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipcode: 94000,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
